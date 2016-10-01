eb1f22928dcc250ab80ac6c3f7d985bb67051922
desaic <desaicwtf@gmail.com>
2016-10-01 10:56:18 -0400

    replace left-handed coordinates with right hand.

--- src/linmath.cpp
+++ src/linmath.cpp
@@ -9,11 +9,10 @@ Eigen::Matrix4f mat4x4_perspective(float y_fov, float aspect, float n, float f)
   m(0, 0) = a / aspect;
   m(1, 1) = a;
 
-  m(2, 2) = -((f + n) / (f - n));
-  m(2, 3) = -1.f;
+  m(2, 2) = -((f + n) / (n - f));
+  m(3, 2) = 1.f;
 
-  m(3, 2) = -((2.f * f * n) / (f - n));
-  m.transposeInPlace();
+  m(2, 3) = ((2.f * f * n) / (n - f));
   return m;
 }
 
@@ -27,27 +26,15 @@ Eigen::Matrix4f mat4x4_look_at(Eigen::Vector3f eye, Eigen::Vector3f center, Eige
   f = center - eye;
   f.normalize();
 
-  s = f.cross(up);
+  s = up.cross(f);
   s.normalize();
 
-  t=s.cross(f);
-
-  m(0,0) = s[0];
-  m(0,1) = t[0];
-  m(0,2) = -f[0];
-
-  m(1,0) = s[1];
-  m(1,1) = t[1];
-  m(1,2) = -f[1];
-
-  m(2,0) = s[2];
-  m(2,1) = t[2];
-  m(2,2) = -f[2];
-
+  t=f.cross(s);
+  m.block(0, 0, 1, 3) = s.transpose();
+  m.block(1, 0, 1, 3) = t.transpose();
+  m.block(2, 0, 1, 3) = f.transpose();
   m(3,3) = 1.f;
-
   mat4x4_translate_in_place(m, -eye[0], -eye[1], -eye[2]);
-  m.transposeInPlace();
   return m;
 }
 
@@ -56,9 +43,6 @@ void mat4x4_translate_in_place(Eigen::Matrix4f & M, float x, float y, float z)
   Eigen::Vector4f t;
   t << x, y, z, 0 ;
   Eigen::Vector4f r;
-  int i;
-  for (i = 0; i < 4; ++i) {
-    r = M.row(i);
-    M(3,i) += r.transpose() * t;
-  }
+  r = M*t;
+  M.block(0, 3, 4, 1) += r;
 }
